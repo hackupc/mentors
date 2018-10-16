1b1a3e87bb553a59d5f0782bcf450346bebe70b8
alvarovilynx <alvaro@vilynx.com>
2018-10-16 19:26:09 +0200

    Removed assets and node watcher from backend.

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -11,8 +11,7 @@ config :hack_upc_mentors_api, HackUpcMentorsApiWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../assets", __DIR__)]]
+  watchers: []
 
 # ## SSL Support
 #
