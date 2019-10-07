9627fb0c199fa4f1a649bc33d12ad0543c2bae5a
Arnau Casas <casassarnau@gmail.com>
2019-10-07 23:13:42 +0200

    Bug fixed

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -19,7 +19,7 @@ import { styles } from './NavBarStyle'
 const ButtonAppBar = (props) => {
   const { classes } = props;
 
-  const [anchorEl, setAnchorEl] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
   function handleMenu(event) {
